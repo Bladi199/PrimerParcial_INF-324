1b08cc89e956d29c06c526c704338f4fc6cdb66c
Bladi199 <bmamanic2@fcpn.edu.bo>
2024-10-03 15:25:48 -0600

    Add files via upload

---
+++ P1/tramites.php
@@ -0,0 +1,47 @@
+<?php include 'header.php'; ?>
+
+<div class="content">
+    <div class="container mt-5">
+        <h1 class="text-center">Trámites y Servicios</h1>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Impuestos Municipales</h5>
+                        <p class="card-text">Consulta y paga tus impuestos municipales.</p>
+                        <a href="impuestos.php" class="btn btn-primary">Ver más</a> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Negocios y Comercio</h5>
+                        <p class="card-text">Realiza tus trámites de licencias comerciales.</p>
+                        <a href="negocios.php" class="btn btn-primary">Ver más</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Catastro y Territorio</h5>
+                        <p class="card-text">Información sobre terrenos y propiedad.</p>
+                        <a href="catastro.php" class="btn btn-primary">Ver más</a> 
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Servicios Sociales</h5>
+                        <p class="card-text">Accede a los programas sociales de La Paz.</p>
+                        <a href="servicios.php" class="btn btn-primary">Ver más</a> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
